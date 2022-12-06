a271577ec1ce7780118588626e515c566d142256
Ekaterina Biyushkina <biyushkina.k@gmail.com>
2022-12-06 16:11:50 +0300

    Program created

---
+++ Task6Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:() ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+    Console.WriteLine($"Число {n} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {n} не является четным");
+}
